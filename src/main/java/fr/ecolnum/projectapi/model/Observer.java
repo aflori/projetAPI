88d98bb588d6b737ddf91a0e33274636d220b669
gilles.reist <gilles.reist@le-campus-numerique.fr>
2023-09-25 11:46:29 +0200

    #6 Observer model created with jpa annotation

---
+++ src/main/java/fr/ecolnum/projectapi/model/Observer.java
@@ -0,0 +1,66 @@
+package fr.ecolnum.projectapi.model;
+
+import jakarta.persistence.*;
+
+/**
+ * This class will be the user that connects to the api
+ */
+@Entity
+public class Observer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
