7b5ef77c21259242ce4499a1de512fada61e67fa
gilles.reist <gilles.reist@le-campus-numerique.fr>
2023-09-25 14:35:34 +0200

    #6 api response added to ObserverController

--- src/main/java/fr/ecolnum/projectapi/controller/ObserverController.java
+++ src/main/java/fr/ecolnum/projectapi/controller/ObserverController.java
@@ -3,6 +3,7 @@ package fr.ecolnum.projectapi.controller;
 import fr.ecolnum.projectapi.model.Observer;
 import fr.ecolnum.projectapi.service.ObserverService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,10 @@ public class ObserverController {
 
     @Operation(summary = "Create an observer", description = "Add a new Observer object to the database.")
     @PostMapping
+    @ApiResponse(
+            description = "Return the created observer and the created HTTP response",
+            responseCode = "201"
+    )
     public ResponseEntity<?> createObserver(@RequestBody Observer observer){
 
         Observer createdObserver =  observerService.createObserver(observer);
@@ -28,6 +33,10 @@ public class ObserverController {
 
     @Operation(summary = "Return all observers", description = "Return the list of all the observers from the database.")
     @GetMapping
+    @ApiResponse(
+            description = "Return observers list and the OK HTTP response",
+            responseCode = "200"
+    )
     public ResponseEntity<?> getAllObservers() {
         return new ResponseEntity<>(observerService.getAllObservers(), HttpStatus.OK);
     }
