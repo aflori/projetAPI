19b9a8c15ca3cd658a72015e143c0667106f156e
aflori <aurelien.flori@le-campus-numerique.fr>
2023-09-25 11:47:52 +0200

    #4 Candidate class created

---
+++ src/main/java/fr/ecolnum/projectapi/model/Candidate.java
@@ -0,0 +1,70 @@
+package fr.ecolnum.projectapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToMany;
+
+import java.util.Set;
+
+@Entity
+public class Candidate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    @ManyToMany
+    private Set<Pool> evaluatedIn;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Pool> getEvaluatedIn() {
+        return evaluatedIn;
+    }
+
+    public void setEvaluatedIn(Set<Pool> evaluatedIn) {
+        this.evaluatedIn = evaluatedIn;
+    }
+
+    @Override
+    public String toString() {
+        return "Candidate{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", evaluatedIn=" + evaluatedIn +
+                '}';
+    }
+}
