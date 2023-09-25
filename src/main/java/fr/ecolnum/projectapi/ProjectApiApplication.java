180ec039fdb634128c513a22b32d0d273ec14b24
aflori <aurelien.flori@le-campus-numerique.fr>
2023-09-25 08:55:39 +0200

    initial commit

---
+++ src/main/java/fr/ecolnum/projectapi/ProjectApiApplication.java
@@ -0,0 +1,13 @@
+package fr.ecolnum.projectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApiApplication.class, args);
+	}
+
+}
