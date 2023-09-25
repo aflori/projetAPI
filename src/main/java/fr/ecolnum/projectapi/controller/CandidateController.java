f5eff2bd3a5146cd52414c388a16d6ab74e104df
aflori <aurelien.flori@le-campus-numerique.fr>
2023-09-25 15:24:07 +0200

    #4 created Candidate controller  documentation (swagger + javadoc)

--- src/main/java/fr/ecolnum/projectapi/controller/CandidateController.java
+++ src/main/java/fr/ecolnum/projectapi/controller/CandidateController.java
@@ -3,6 +3,7 @@ package fr.ecolnum.projectapi.controller;
 import fr.ecolnum.projectapi.model.Candidate;
 import fr.ecolnum.projectapi.service.CandidateService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +22,25 @@ public class CandidateController {
     @Autowired
     private CandidateService service;
 
+    /**
+     * function made to answer a post request on serveur
+     *
+     * @param candidate candidate that must be created on database
+     * @return the candidate created
+     * @author aflori
+     */
     @PostMapping
+    @Operation(
+            summary = "Create a new candidate on database",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "first and last name of a candidate",
+                    required = true
+            )
+    )
+    @ApiResponse(
+            description = "Candidate created by server",
+            responseCode = "201"
+    )
     public ResponseEntity<Candidate> createCandidate(@RequestBody Candidate candidate) {
         Candidate createdCandidate = service.createCandidate(candidate);
 
