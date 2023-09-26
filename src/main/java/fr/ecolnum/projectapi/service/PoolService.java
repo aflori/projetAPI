3e63e171d3a0bee8fa40e7deb05bb8c9a66060a7
MarcBoillot <marc.boillot@le-campus-numerique.fr>
2023-09-26 15:53:01 +0200

    #9-#28-#29 feature for unification to add all objects in pool

--- src/main/java/fr/ecolnum/projectapi/service/PoolService.java
+++ src/main/java/fr/ecolnum/projectapi/service/PoolService.java
@@ -1,12 +1,19 @@
 package fr.ecolnum.projectapi.service;
 
+import fr.ecolnum.projectapi.model.Candidate;
+import fr.ecolnum.projectapi.model.Criteria;
 import fr.ecolnum.projectapi.model.Observer;
 import fr.ecolnum.projectapi.model.Pool;
+import fr.ecolnum.projectapi.repository.ObserverRepository;
 import fr.ecolnum.projectapi.repository.PoolRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Optional;
+import java.util.Set;
 
 /**
  * use method CRUD for pool
@@ -28,11 +35,33 @@ public class PoolService {
        return poolRepository.save(pool);
     }
 
-    public Pool modifyPool(Pool pool){
-       return poolRepository.save(pool);
+    /**
+     *
+     * @param poolId recuperation id of pool
+     * @param poolModification add to pools an observer or a criteria or candidate with poolId
+     */
+    public void modifyPool(int poolId, Pool poolModification) {
+        Pool modifiedPool = poolRepository.getReferenceById(poolId);
+
+        Set<Candidate> candidateSet = poolModification.getEvaluates();
+        if (candidateSet != null) {
+            for (Candidate candidate : poolModification.getEvaluates()) {
+                modifiedPool.getEvaluates().add(candidate);
+            }
+        }
+        Set<Observer> observerSet = poolModification.getContainedObservers();
+        if (observerSet != null) {
+        for (Observer observer : poolModification.getContainedObservers()) {
+            modifiedPool.getContainedObservers().add(observer);
+            }
+        }
+        Set<Criteria> criteriaSet = poolModification.getContainedCriterias();
+        if (criteriaSet != null) {
+            for (Criteria criteria : criteriaSet) {
+                modifiedPool.getContainedCriterias().add(criteria);
+            }
+        }
+        poolRepository.save(modifiedPool);
     }
-    /*public String addObserverToPool(Observer){
-        return null;
-    }*/
 }
 
