ddb0949b3833c142d59d3f1f5eeb86cd679f7a3a
aflori <aurelien.flori@le-campus-numerique.fr>
2023-09-26 15:52:51 +0200

    #4 removing unuseful method

--- src/main/java/fr/ecolnum/projectapi/repository/CandidateRepository.java
+++ src/main/java/fr/ecolnum/projectapi/repository/CandidateRepository.java
@@ -13,5 +13,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CandidateRepository extends JpaRepository<Candidate, Integer> {
 
-    Candidate findTopByOrderByIdDesc();
+//    Candidate findTopByOrderByIdDesc();
 }
