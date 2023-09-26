a135b724af0228ba412f32d863ed65a7ff0d7362
aflori <aurelien.flori@le-campus-numerique.fr>
2023-09-26 16:04:48 +0200

    #4 deleted static function and using utility class static function instead

--- src/main/java/fr/ecolnum/projectapi/service/CandidateService.java
+++ src/main/java/fr/ecolnum/projectapi/service/CandidateService.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Optional;
+import static fr.ecolnum.projectapi.util.FileUtility.*;
 
 /**
  * Service for candidate action
@@ -49,50 +46,5 @@ public class CandidateService {
         return repository.save(newCandidateSaved);
     }
 
-    private static void changeFileName(String oldName, String newName) {
-        File oldF = new File(oldName);
-        File newF = new File(newName);
-        oldF.renameTo(newF);
-    }
 
-    private static String extractPhotoExtension(MultipartFile photoCandidate) throws MultipartFileIsNotImageException {
-
-        String photoType = photoCandidate.getContentType();
-        if (photoType == null) {
-            throw new MultipartFileIsNotImageException("no file found");
-        }
-        String fileType = photoType.substring(0, 5);
-        String fileExtension = photoType.substring(6);
-
-        if (fileType.equals("image")) {
-            return '.' + fileExtension;
-        }
-        throw new MultipartFileIsNotImageException("not an image file");
-    }
-
-    private static void writePhotoIn(MultipartFile photoCandidate, String fileName) throws FileNotUpdatableException {
-        try {
-            FileOutputStream photoFile = new FileOutputStream(fileName, false);
-            byte[] photoData = photoCandidate.getBytes();
-            photoFile.write(photoData);
-            photoFile.close();
-        } catch (IOException e) {
-            //can't happen, File class did create file
-            throw new FileNotUpdatableException();
-        }
-    }
-
-    private static void createEmptyFileByName(String fileName) throws FileNotUpdatableException {
-        try {
-            File creationFile = new File(fileName);
-            if (creationFile.exists()) {
-                creationFile.delete();
-            }
-            creationFile.createNewFile();
-
-        } catch (Exception e) {
-            //could not create file
-            throw new FileNotUpdatableException();
-        }
-    }
 }
