cc4ebf7c88c66dce35b4e4f6f187e283f20f6dbd
MarcBoillot <marc.boillot@le-campus-numerique.fr>
2023-09-28 09:04:11 +0200

    #2 add others classes from criteria

--- src/test/java/fr/ecolnum/projectapi/ProjectApiApplicationTests.java
+++ src/test/java/fr/ecolnum/projectapi/ProjectApiApplicationTests.java
@@ -1,7 +1,11 @@
 package fr.ecolnum.projectapi;
 
+import fr.ecolnum.projectapi.model.Candidate;
+import fr.ecolnum.projectapi.model.Pool;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class ProjectApiApplicationTests {
@@ -9,5 +13,14 @@ class ProjectApiApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	public void testCreateCandidateToPool() {
+		Pool pool = new Pool();
+		Candidate candidate = new Candidate("Aurelien");
 
+		// Inscrivez le participant à la piscine
+		pool.getName(candidate);
+
+		assertTrue(pool.isPresent(candidate));
+	}
 }
