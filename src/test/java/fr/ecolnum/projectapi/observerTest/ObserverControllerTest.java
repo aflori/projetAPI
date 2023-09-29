961e9d4ffa6a22b40e87c90deea3eb66d120225f
aflori <aurelien.flori@le-campus-numerique.fr>
2023-09-29 13:26:29 +0200

    test: added second test on ObserverController

--- src/test/java/fr/ecolnum/projectapi/observerTest/ObserverControllerTest.java
+++ src/test/java/fr/ecolnum/projectapi/observerTest/ObserverControllerTest.java
@@ -12,8 +12,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.*;
@@ -57,6 +58,50 @@ public class ObserverControllerTest {
 
 
         controllerResponse = observerController.createObserver(observerDto);
-        assert(controllerResponse.getStatusCode()==HttpStatus.CONFLICT);
+        assert (controllerResponse.getStatusCode() == HttpStatus.CONFLICT);
+    }
+
+    @Test
+    public void testEndpointGetAllObservers() {
+        //mock
+        List<ObserverDto> answer = List.of(
+                new ObserverDto(1, "toto", "totobis"),
+                new ObserverDto(2, "toto2", "toto2bis"),
+                new ObserverDto(10, "toto3", "toto3bis")
+        );
+        when(observerService.getAllObservers())
+                .thenReturn(answer);
+
+
+        ResponseEntity<?> endpointResult = observerController.getAllObservers();
+
+        int numberObserver = checkObserverAndGetTheirNumver(endpointResult);
+        assertEquals(numberObserver, 3);
+
+    }
+
+    private static int checkObserverAndGetTheirNumver(ResponseEntity<?> endpointResult) {
+
+        assert (endpointResult.getStatusCode() == HttpStatus.OK);
+        //check if its a good instance
+        Object contentResponseEntity = endpointResult.getBody();
+        assert (contentResponseEntity instanceof Iterable<?>);
+
+        //cast to avoid problems
+        Iterable<?> contentIterables = (Iterable<?>) contentResponseEntity;
+
+
+        int numberObserver = 0;
+        for (Object observer : contentIterables) {
+            //check type
+            assert (observer instanceof ObserverDto);
+            //cast
+            ObserverDto trueObserverDto = (ObserverDto) observer;
+            //check ids
+            assert (trueObserverDto.getId() == 1 || trueObserverDto.getId() == 2 || trueObserverDto.getId() == 10);
+
+            numberObserver++;
+        }
+        return numberObserver;
     }
 }
