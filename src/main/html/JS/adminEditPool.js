f9130d7879fdc69eb1eb33ae4f24b741a90a6efa
asmuro1 <f.aurelien@yahoo.fr>
2023-10-01 14:42:53 +0200

    refacto JS and html into 1 demo page

--- src/main/html/JS/adminEditPool.js
+++ src/main/html/JS/adminEditPool.js
@@ -1,9 +1,61 @@
+// list pool
+function showErrorMessagePoolList(codeError, bodyTag) {
+    const h3 = document.createElement("h3");
+    h3.textContent = "ERROR CODE: " + codeError + " - no data found";
+    bodyTag.appendChild(h3);
+}
+function createTagWithoutTextAndAddAsChild(tagName, tagParent) {
+    const tagCreated = document.createElement(tagName);
+    tagParent.appendChild(tagCreated);
+}
+function createTagWithTextAndAddAsChild(tagName, tagTextContent, parentTag) {
+    const newTag = document.createElement(tagName);
+    newTag.textContent = tagTextContent;
+    parentTag.appendChild(newTag);
+}
+
+function getPoolTag(pool) {
+    const poolContent = document.createElement("article");
+
+    createTagWithTextAndAddAsChild("h2",pool.name, poolContent);
+    createTagWithTextAndAddAsChild("small","id: " + pool.id, poolContent);
+    createTagWithoutTextAndAddAsChild("br", poolContent);
+    createTagWithTextAndAddAsChild("span", "date de début: " + pool.startDate, poolContent);
+    createTagWithoutTextAndAddAsChild("br", poolContent);
+    createTagWithTextAndAddAsChild("span", "date de fin des test: " +pool.endDate, poolContent);
+    createTagWithoutTextAndAddAsChild("br", poolContent);
+    createTagWithTextAndAddAsChild("i", "à " + pool.location, poolContent);
+
+    return poolContent;
+}
+
+function showPoolsOnStream(arrayData, bodyTag) {
+    for (const pool of arrayData) {
+        console.log(pool);
+        const tag = getPoolTag(pool);
+        bodyTag.appendChild(tag);
+    }
+}
+
+function showAllPools() {
+    ApiInteraction.getAllPools()
+        .then((articleContent) => {
+            const body = document.querySelector("#poolsList");
+            if (typeof articleContent === "number") {
+                showErrorMessagePoolList(articleContent, body);
+            } else {
+                showPoolsOnStream(articleContent, body);
+            }
+        });
+}
+
+// form functions
 function getInputTagValueByName(name) {
     const tag = document.querySelector('input[name="' + name + '"]');
     return tag.value;
 }
 
-function showErrorMessage(codeNumber) {
+function showErrorMessagePoolForm(codeNumber) {
     console.log("Impossible de créer la piscine, code d'erreur : " + codeNumber);
 }
 
@@ -39,7 +91,7 @@ function createPoolOrModifyPool(event) {
         responseValue.then(
             (JSON) => {
                 if (typeof JSON === "number") {
-                    showErrorMessage(JSON);
+                    showErrorMessagePoolForm(JSON);
                 } else {
                     window.location.href = "../htmlFile/listPiscine.html";
                 }
@@ -51,8 +103,11 @@ function createPoolOrModifyPool(event) {
 
 
 function createPoolListener() {
-    const formTag = document.querySelector("button[type=submit]");
+    const formTag = document.querySelector("#poolCreationForm button");
     formTag.addEventListener("click", createPoolOrModifyPool);
 }
 
-window.addEventListener("DOMContentLoaded", createPoolListener);
+window.addEventListener("DOMContentLoaded", () => {
+    showAllPools();
+    createPoolListener();
+});
