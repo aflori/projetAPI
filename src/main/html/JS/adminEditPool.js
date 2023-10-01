759571c44e9f5470af170bf86598cd978b6bdf3c
asmuro1 <f.aurelien@yahoo.fr>
2023-10-01 12:15:01 +0200

    refacto + check

--- src/main/html/JS/adminEditPool.js
+++ src/main/html/JS/adminEditPool.js
@@ -6,35 +6,53 @@ function getInputTagValueByName(name) {
 function showErrorMessage(codeNumber) {
     console.log("Impossible de créer la piscine, code d'erreur : " + codeNumber);
 }
-function createPool(event) {
+
+function hasNoEmptyMandatoryField(pool) {
+    return pool.name !== "" && pool.startDate !== "" && pool.endDate !== "" && pool.location !== "";
+}
+
+function createPoolOrModifyPool(event) {
     event.preventDefault();
     const pool = {}
+    let isModifyingAPool = false;
 
     const id = getInputTagValueByName("id");
     if (id !== "") {
         pool.id = id;
+        isModifyingAPool = true;
     }
 
     pool.name = getInputTagValueByName("name");
     pool.startDate = getInputTagValueByName("startDate");
     pool.endDate = getInputTagValueByName("endDate");
     pool.location = getInputTagValueByName("location");
-    ApiInteraction.createPool(pool).then(
-        (JSON) => {
-            if (typeof JSON === "number") {
-                showErrorMessage(JSON);
-            }
-            else {
-                window.location.href = "../htmlFile/administrationPiscine.html";
-            }
+
+    if (hasNoEmptyMandatoryField(pool)) {
+        let responseValue;
+
+        if (isModifyingAPool) {
+            responseValue = ApiInteraction.createPool(pool);
+        } else {
+            responseValue = ApiInteraction.modifyPool(pool);
         }
-    );
+
+        responseValue.then(
+            (JSON) => {
+                if (typeof JSON === "number") {
+                    showErrorMessage(JSON);
+                } else {
+                    window.location.href = "../htmlFile/listPiscine.html";
+                }
+            }
+        );
+
+    }
 }
 
 
 function createPoolListener() {
-    const formTag = document.querySelector("button");
-    formTag.addEventListener("click", createPool);
+    const formTag = document.querySelector("button[type=submit]");
+    formTag.addEventListener("click", createPoolOrModifyPool);
 }
 
 window.addEventListener("DOMContentLoaded", createPoolListener);
